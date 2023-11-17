c13e0cf9af6f0e030e2b0d853eedab4e967d3693
unknown <rahulpradeep061@gmail.com>
2023-11-17 17:15:30 +0530

    update

--- src/app/api/posts/all/route.ts
+++ src/app/api/posts/all/route.ts
@@ -16,7 +16,7 @@ export async function GET() {
             });
         return NextResponse.json(response, {status: 200})
     }catch(error){
-        return NextResponse.json({message: 'could not gettin post'}, {status: 500})
+        return NextResponse.json({message: 'could not fetching post'}, {status: 500})
     }
     
 }
