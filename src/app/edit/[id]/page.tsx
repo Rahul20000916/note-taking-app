9f24af9912a6dad0cde4504441a8187a55a9a722
unknown <rahulpradeep061@gmail.com>
2023-11-18 20:35:08 +0530

    update edit

--- src/app/edit/[id]/page.tsx
+++ src/app/edit/[id]/page.tsx
@@ -1,4 +1,71 @@
-"use client";
+// "use client";
+// import { FormInputPost } from "@/types";
+// import FormPost from "@/components/FormPost";
+// import { SubmitHandler } from "react-hook-form";
+// import { useMutation, useQuery } from "@tanstack/react-query";
+// import { FC } from "react";
+// import axios from "axios";
+// import { useRouter } from "next/navigation";
+// import BackButton from "@/components/BackButton";
+
+// interface EditNotePageProps {
+//   params: {
+//     id: string;
+//   };
+// }
+
+// const EditNotePage: FC<EditNotePageProps> = ({ params }) => {
+//   const { id } = params;
+//   const router = useRouter();
+//   const { data: dataPost, isLoading: isLoadingPost } = useQuery({
+//     queryKey: ["posts", id],
+//     queryFn: async () => {
+//       const response = await axios.get(`/api/posts/${id}`);
+//       return response.data;
+//     },
+//   });
+
+//   const { mutate: updatPost, isPending: isPendingSubmit } = useMutation({
+//     mutationFn: (newPost: FormInputPost) => {
+//       return axios.patch(`/api/posts/${id}`, newPost);
+//     },
+//     onError: (error) => {
+//       console.error(error);
+//     },
+//     onSuccess: () => {
+//       router.push("/");
+//       router.refresh();
+//     },
+//   });
+
+//   const handleEditPost: SubmitHandler<FormInputPost> = (data) => {
+//     updatPost(data);
+//   };
+
+//   if (isLoadingPost) {
+//     return (
+//       <div className="text-center">
+//           <span className="loading loading-spinner loading-lg"></span>
+//         </div>
+//     );
+//   }
+//   return (
+//     <div>
+//       <BackButton />
+//       <h1 className="text-2xl my-4 font-bold text-center">EDIT NOTE</h1>
+//       <FormPost
+//         isPendingSubmit={isPendingSubmit}
+//         submit={handleEditPost}
+//         initialValue={dataPost}
+//         isEditing={true}
+//       />
+//     </div>
+//   );
+// };
+
+// export default EditNotePage;
+'use client'
+import { useEffect, useState } from "react";
 import { FormInputPost } from "@/types";
 import FormPost from "@/components/FormPost";
 import { SubmitHandler } from "react-hook-form";
@@ -7,6 +74,16 @@ import { FC } from "react";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 import BackButton from "@/components/BackButton";
+
+interface ApiResponse {
+  id: string;
+  title: string;
+  content: string;
+  tag: {
+    id: string;
+    name: string;
+  };
+}
 
 interface EditNotePageProps {
   params: {
@@ -17,15 +94,10 @@ interface EditNotePageProps {
 const EditNotePage: FC<EditNotePageProps> = ({ params }) => {
   const { id } = params;
   const router = useRouter();
-  const { data: dataPost, isLoading: isLoadingPost } = useQuery({
-    queryKey: ["posts", id],
-    queryFn: async () => {
-      const response = await axios.get(`/api/posts/${id}`);
-      return response.data;
-    },
-  });
+  const [dataPost, setDataPost] = useState<ApiResponse>();
+  const [isLoadingPost, setIsLoadingPost] = useState(true);
 
-  const { mutate: updatPost, isPending: isPendingSubmit } = useMutation({
+  const { mutate: updatePost, isPending: isPendingSubmit } = useMutation({
     mutationFn: (newPost: FormInputPost) => {
       return axios.patch(`/api/posts/${id}`, newPost);
     },
@@ -39,16 +111,33 @@ const EditNotePage: FC<EditNotePageProps> = ({ params }) => {
   });
 
   const handleEditPost: SubmitHandler<FormInputPost> = (data) => {
-    updatPost(data);
+    updatePost(data);
   };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get<ApiResponse>(`/api/posts/${id}`);
+        setDataPost(response.data);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setIsLoadingPost(false);
+      }
+    };
+
+    fetchData();
+  }, [id]);
+
 
   if (isLoadingPost) {
     return (
       <div className="text-center">
-          <span className="loading loading-spinner loading-lg"></span>
-        </div>
+        <span className="loading loading-spinner loading-lg"></span>
+      </div>
     );
   }
+
   return (
     <div>
       <BackButton />
@@ -56,7 +145,7 @@ const EditNotePage: FC<EditNotePageProps> = ({ params }) => {
       <FormPost
         isPendingSubmit={isPendingSubmit}
         submit={handleEditPost}
-        initialValue={dataPost}
+        initialValue ={dataPost}
         isEditing={true}
       />
     </div>
