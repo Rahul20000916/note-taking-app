29d4d24517a15ac2b934b59b8015e35c2fc53724
unknown <rahulpradeep061@gmail.com>
2023-11-19 12:26:12 +0530

    up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,31 +5,31 @@ import { PenSquare } from 'lucide-react';
 import { useState, useEffect } from 'react';
 
 
-interface Post {
-  id: string;
-  title: string;
-  tag: {
-    id: string;
-    name: string;
-  };
-  content: string;
-}
+// interface Post {
+//   id: string;
+//   title: string;
+//   tag: {
+//     id: string;
+//     name: string;
+//   };
+//   content: string;
+// }
 
 export default function Home() {
-  const [posts, setPosts] = useState<Post[] | null>(null);
+  //const [posts, setPosts] = useState<Post[] | null>(null);
 
-  useEffect(() => {
-    async function fetchPosts() {
-      try {
-        const response = await axios.get('/api/getPosts');
-        console.log(response.data, 'data fetching');
-        setPosts(response.data);
-      } catch (error) {
-        console.error('Error fetching posts:', error);
-      }
-    }
-    fetchPosts();
-  }, []);
+  // useEffect(() => {
+  //   async function fetchPosts() {
+  //     try {
+  //       const response = await axios.get('/api/getPosts');
+  //       console.log(response.data, 'data fetching');
+  //       setPosts(response.data);
+  //     } catch (error) {
+  //       console.error('Error fetching posts:', error);
+  //     }
+  //   }
+  //   fetchPosts();
+  // }, []);
 
   return (
     <main className='grid items-center justify-center md:grid-cols-2 lg:grid-cols-3 gap-2 mt-10'>
@@ -37,10 +37,10 @@ export default function Home() {
         <PenSquare />
         Notes
       </a>
-      {posts &&
+      {/* {posts &&
         posts.map((post: Post) => (
            <PostCard key={post.id} post={post} /> 
-        ))}
+        ))} */}
     </main>
   );
 }
