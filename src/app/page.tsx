190a099478a576644cfde8138cdb9e5fb7bd3484
unknown <rahulpradeep061@gmail.com>
2023-11-19 17:30:01 +0530

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,49 +1,7 @@
 'use client'
-// import PostCard from '@/components/PostCard'
-// import axios from 'axios';
-// import { useState, useEffect } from 'react';
-
-// interface Post {
-//   id: string;
-//   title: string;
-//   tag: {
-//     id: string;
-//     name: string;
-//   };
-//   content: string;
-// }
-
-// export default function Home() {
-//   const [posts, setPosts] = useState<Post[] | null>(null);
-
-//   useEffect(() => {
-//     async function fetchPosts() {
-//       try {
-//         const response = await axios.get('/api/posts/all');
-//         console.log(response.data, 'data fetching');
-//         setPosts(response.data);
-//       } catch (error) {
-//         console.error('Error fetching posts:', error);
-//       }
-//     }
-//     fetchPosts();
-//   }, []);
-
-//   return (
-//     <main className='grid items-center justify-center md:grid-cols-2 lg:grid-cols-3 gap-2 mt-10'>
-       
-//       {posts &&
-//         posts.map((post: Post) => (
-//            <PostCard key={post.id} post={post} /> 
-//         ))}
-//     </main>
-//   );
-// }
-
-// Import necessary dependencies
-import useSWR from 'swr';
-import PostCard from '@/components/PostCard';
-import { useState } from 'react';
+import PostCard from '@/components/PostCard'
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 interface Post {
   id: string;
@@ -56,21 +14,27 @@ interface Post {
 }
 
 export default function Home() {
-  const { data: posts, error } = useSWR('/api/posts/all', async (url) => {
-    const response = await fetch(url);
-    const data = await response.json();
-    return data;
-  });
+  const [posts, setPosts] = useState<Post[] | null>(null);
 
-  if (error) {
-    console.error('Error fetching posts:', error);
-  }
+  useEffect(() => {
+    async function fetchPosts() {
+      try {
+        const response = await axios.get('/api/posts/all');
+        console.log(response.data, 'data fetching');
+        setPosts(response.data);
+      } catch (error) {
+        console.error('Error fetching posts:', error);
+      }
+    }
+    fetchPosts();
+  }, []);
 
   return (
     <main className='grid items-center justify-center md:grid-cols-2 lg:grid-cols-3 gap-2 mt-10'>
+       
       {posts &&
         posts.map((post: Post) => (
-          <PostCard key={post.id} post={post} />
+           <PostCard key={post.id} post={post} /> 
         ))}
     </main>
   );
