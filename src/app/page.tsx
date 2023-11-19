f17e6efa9af9b86e1e71fb9a69053ab38d2cd60f
unknown <rahulpradeep061@gmail.com>
2023-11-19 16:03:03 +0530

    update all

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 'use client'
 import PostCard from '@/components/PostCard'
 import axios from 'axios';
-import { PenSquare } from 'lucide-react';
 import { useState, useEffect } from 'react';
-import { fetchPosts } from './getAll/fetchPosts';
 
 interface Post {
   id: string;
@@ -15,24 +13,25 @@ interface Post {
   content: string;
 }
 
-export default function Home({ posts }: { posts: Post[] }) {
-  // const [posts, setPosts] = useState<Post[] | null>(null);
+export default function Home() {
+  const [posts, setPosts] = useState<Post[] | null>(null);
 
-  // useEffect(() => {
-  //   async function fetchData() {
-  //     try {
-  //       const data = await fetchPosts(); 
-  //       console.log(data, 'data fetching');
-  //       setPosts(data);
-  //     } catch (error) {
-  //       console.error('Error fetching posts:', error);
-  //     }
-  //   }
-  //   fetchData();
-  // }, []);
+  useEffect(() => {
+    async function fetchPosts() {
+      try {
+        const response = await axios.get('/api/posts/all');
+        console.log(response.data, 'data fetching');
+        setPosts(response.data);
+      } catch (error) {
+        console.error('Error fetching posts:', error);
+      }
+    }
+    fetchPosts();
+  }, []);
 
   return (
     <main className='grid items-center justify-center md:grid-cols-2 lg:grid-cols-3 gap-2 mt-10'>
+       
       {posts &&
         posts.map((post: Post) => (
            <PostCard key={post.id} post={post} /> 
@@ -40,18 +39,3 @@ export default function Home({ posts }: { posts: Post[] }) {
     </main>
   );
 }
-
-export const getServerSideProps = async () => {
-  try {
-    const data = await fetchPosts();
-    console.log(data, 'data fetching');
-    return {
-      props: { posts: data },
-    };
-  } catch (error) {
-    console.error('Error fetching posts:', error);
-    return {
-      props: { posts: [] }, // Return an empty array or handle the error as needed
-    };
-  }
-};
